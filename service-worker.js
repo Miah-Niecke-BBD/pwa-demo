3b50c360310eb7f8ac8fb9764166bbcf975c9b18
Miah-Niecke Dreecmeier <miah.dreeckmeier@bbd.co.za>
2026-05-27 03:30:48 +0200

    Expanded functionality

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
-const CACHE_NAME = "pwa-demo-v1";
+const CACHE_NAME = "pwa-demo-v2";
+
 const urlsToCache = [
   "/pwa-demo/",
-  "/pwa-demo/index.html"
+  "/pwa-demo/index.html",
+  "/pwa-demo/manifest.json",
+  "/pwa-demo/icon.png"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
